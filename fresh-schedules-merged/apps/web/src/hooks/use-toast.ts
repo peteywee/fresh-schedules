d69ec88d303ac94d87920b45f7362ac0b17466d2
Patrick craven <patricktssllc@gmail.com>
2025-10-17 04:25:20 -0500

    Replace with monorepo (previous tree archived in /legacy) (#38)
    * inital upload
    
    * chore: archive previous firebase-stump tree under /legacy
    
    * Refactor useToast hook to manage state correctly

--- fresh-schedules-merged/apps/web/src/hooks/use-toast.ts
+++ fresh-schedules-merged/apps/web/src/hooks/use-toast.ts
@@ -182,7 +182,26 @@ function useToast() {
         listeners.splice(index, 1)
       }
     }
-  }, [state])
+  }, [ function useToast() {
+   const [state, setState] = React.useState<State>(memoryState)
+ 
+   React.useEffect(() => {
+     listeners.push(setState)
+     return () => {
+       const index = listeners.indexOf(setState)
+       if (index > -1) {
+         listeners.splice(index, 1)
+       }
+     }
+-  }, [state])
++  }, [])
+ 
+   return {
+     ...state,
+     toast,
+     dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+   }
+ }])
 
   return {
     ...state,
